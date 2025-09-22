7fbfb56c290de8007ce17ff4ee3ef2dbd5c00d51
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2025-09-22 11:19:07 -0400

    Initial Commit

---
+++ Project Radio/Project_RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_RadioApp.swift
+//  Project Radio
+//
+//  Created by Autumn Williams on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
