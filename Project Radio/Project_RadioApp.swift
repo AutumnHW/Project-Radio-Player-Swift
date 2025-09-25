153f7eec68ed9fc5575fecd00b751d9166a82ca7
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2025-09-25 00:58:53 -0400

    added debug app icons, did some frontend design

--- Project Radio/Project_RadioApp.swift
+++ Project Radio/Project_RadioApp.swift
@@ -19,11 +19,11 @@ struct Project_RadioApp: App {
             TabView {
                 prfmView()
                     .tabItem { Label("ProjectFM", systemImage: "mic") }
-                RuggedRaw94()
+                ruggedraw94View()
                     .tabItem { Label("RuggedRaw94", systemImage: "mic") }
                 dogstarfmView()
                     .tabItem { Label("DogStarFM", systemImage: "mic") }
-            }
+            }.padding(10)
         }
     }
 }
---
+++ Project Radio/Views/ruggedraw94View.swift
@@ -0,0 +1,37 @@
+//
+//  radioPlayerView.swift
+//  Project Radio
+//
+//  Created by Autumn Williams on 9/22/25.
+//
+
+import SwiftUI
+
+struct ruggedraw94View: View {
+    var body: some View {
+        VStack {
+            createButton(label: "Play") {
+                AudioManager.shared.playRadio(source:"https://radio.projectradio.org/listen/ruggedraw94/radio.mp3")
+            }
+            
+            
+            createButton(label: "Pause") {
+                AudioManager.shared.pause()
+            }
+        }
+    }
+    
+    private func createButton(
+        label: String,
+        action: @escaping () -> Void
+    ) -> some View {
+        return Button(label, action: action)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(20)
+            .foregroundColor(.white)
+    }
+}
+#Preview {
+    dogstarfmView()
+}
